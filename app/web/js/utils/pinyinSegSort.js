8e8b711be4fdc61b139f51fbc0d1621bd1f69669
zmh3788 <710101991@qq.com>
2018-12-07 20:31:57 +0800

    new transactions

--- app/web/js/utils/pinyinSegSort.js
+++ app/web/js/utils/pinyinSegSort.js
@@ -1,12 +1,13 @@
-/** @file
+/**
+ *  @file
  *  @author zhouminghui
  *  2018.12.5
- *  中文的分类并返回心的列表
+ *  中文的分类并返回新的列表
  */
 
 import contactCompare from './contactCompare';
 
-function pinyinSegSort(arry) {
+export default function pinyinSegSort(arry) {
     let addressclass = arry;
     if (addressclass === undefined) {
         return;
@@ -17,6 +18,3 @@ function pinyinSegSort(arry) {
     let newAddressclass = addressclass.sort(contactCompare('srt'));
     return newAddressclass;
 }
-
-export default pinyinSegSort;
-
