a5df93dea0032e104e7425fda6a6d43858def878
hzz780 <hzz780@qq.com>
2019-05-10 11:57:21 +0800

    remove uselese things

--- app/router.js
+++ app/router.js
@@ -7,7 +7,7 @@ module.exports = app => {
     const {router, controller} = app;
     router.get('/', controller.home.index);
 
-    router.get('/test', controller.test.test);
+    // router.get('/test', controller.test.test);
 
     router.get('/transactiondetail', controller.home.transactionDetail);
 
